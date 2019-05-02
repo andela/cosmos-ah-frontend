b67dc8a330b65031c67c9e559d47d0f2d401be00
oluwaseyi000 <adebajo.oluwaseyi@gmail.com>
2019-05-02 21:56:30 +0100

    #164796851 linkedin auth
    linkedin auth
    
    [finishes #164796851]

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -6,6 +6,7 @@ class Login extends Component {
       <Fragment>
         <h3>Log in</h3>
         <p>Welcome back!</p>
+        <button> <a href='https://author-haven-stage.herokuapp.com/api/v1/auth/linkedin'>Sign in</a> </button> <br/><br/>
         <form>
           <div>
             <input type="text" placeholder="Email address" />
