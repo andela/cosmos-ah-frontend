02f635ac14cd87f2200f698608f9c8a2eb388302
chikeozulumba <o.devcode@gmail.com>
2019-05-07 16:13:59 +0100

    165856948-feature: Create Profile Components
    - create components
    - rename files
    - fix indentation
    
    [Delivers #165856948]

---
+++ src/components/ProfileComponent/Social/Stats/Followers.jsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+import { Paragraph } from '../../../shared/Text/Text';
+
+const Followers = ({ followers, paragraphStyle }) => (
+  <Fragment>
+    <Paragraph paragraphStyle={paragraphStyle}>{followers || 0} followers</Paragraph>
+	</Fragment>
+);
+
+export default Followers;
---
+++ src/components/ProfileComponent/Social/Stats/Following.jsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+import { Paragraph } from '../../../shared/Text/Text';
+
+const Following = ({ following, paragraphStyle }) => (
+  <Fragment>
+    <Paragraph paragraphStyle={paragraphStyle}>{following || 10} following</Paragraph>
+	</Fragment>
+);
+
+export default Following;
