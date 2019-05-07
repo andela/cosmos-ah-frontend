06439ed1255c77f2ff759ea2db4dd39a2a8829e7
oluwaseyi000 <adebajo.oluwaseyi@gmail.com>
2019-05-07 11:02:31 +0100

    #164796851 social authentication
    user can sign in using social media
    
    [finishes #164796851]

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -5,27 +5,34 @@ import styled from 'styled-components';
 const IconStyle = styled.div`
   margin: 25px 0;
 `;
-
+const socialAuth = type => {
+  try {
+    window.location.href = `https://author-haven-stage.herokuapp.com/api/v1/auth/${type}`;
+  } catch (error) {
+    throw error;
+  }
+};
 const SocialButton = ({ children }) => (
   <Grid.Column mobile={3} tablet={2} computer={1}>
     {children}
   </Grid.Column>
 );
 
+
 const Icon = () => (
   <IconStyle>
     <Grid textAlign='center'>
       <SocialButton>
-        <Button basic color='blue' size='massive' circular icon='facebook' />
+        <Button basic color='blue' size='massive' circular icon='facebook' onClick={() => socialAuth('facebook')}/>
       </SocialButton>
-      <SocialButton>
-        <Button basic color='blue' size='massive' circular icon='twitter' />
+      <SocialButton >
+        <Button basic color='blue' size='massive' circular icon='twitter' onClick={() => socialAuth('twitter')}/>
       </SocialButton>
-      <SocialButton>
-        <Button basic color='red' size='massive' circular icon='google plus' />
+      <SocialButton >
+        <Button basic color='red' size='massive' circular icon='google plus' onClick={() => socialAuth('google')}/>
       </SocialButton>
-      <SocialButton>
-        <Button basic color='blue' size='massive' circular icon='linkedin' />
+      <SocialButton >
+        <Button basic color='blue' size='massive' circular icon='linkedin' onClick={() => socialAuth('linkedin')}/>
       </SocialButton>
     </Grid>
   </IconStyle >
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const Home = () => (
-  <div>
-    <h3>Welcome to Authors Haven</h3>
-    <p>A place for creative minds</p>
-  </div>
-);
+const Home = () => {
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    if (urlParams.has('token')) {
+      localStorage.signinUser = urlParams.get('token');
+    }
+  });
+
+  return (
+    <div>
+      <h3>Welcome to Authors Haven</h3>
+      <p>A place for creative minds</p>
+    </div>
+  );
+};
 
 export default Home;
