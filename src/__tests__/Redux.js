6ad53e484a5bd62df8ae39f1a4e52b4e44ca86e3
Tolulope Olaniyan <toluola7@gmail.com>
2019-05-08 15:24:56 +0100

    164796850-feature (loginReduxFunctionality): Setting up the Redux for login Functionality
     - A login redux action will be dispached when login is successful
    
    - A login error action will be dispached when login is not successful
    
    [Delivers #164796850]

---
+++ src/__tests__/Redux.js
@@ -0,0 +1,48 @@
+import configureStore from 'redux-mock-store';
+import { signInSuccess, signInError, successDispatch } from '../state/auth/actions';
+
+const middlewares = [];
+const mockStore = configureStore(middlewares);
+
+const signIn = {
+  data: 'ok',
+};
+
+const signInErrorData = {
+  data: 'error',
+};
+
+const signinSuccessAction = signInSuccess(signIn);
+const signinErrorAction = signInError(signInErrorData);
+
+it('should dispatch signin success action', () => {
+  const initialState = {};
+  const store = mockStore(initialState);
+
+  store.dispatch(signinSuccessAction);
+
+  const actions = store.getActions();
+  const expectedPayload = {
+    payload: {
+      data: 'ok',
+    },
+    type: 'SIGN_IN_SUCCESS',
+  };
+  expect(actions).toEqual([expectedPayload]);
+});
+
+it('should dispatch signin error action', () => {
+  const initialState = {};
+  const store = mockStore(initialState);
+
+  store.dispatch(signinErrorAction);
+
+  const actions = store.getActions();
+  const expectedPayload = {
+    payload: {
+      data: 'error',
+    },
+    type: 'SIGN_IN_ERROR',
+  };
+  expect(actions).toEqual([expectedPayload]);
+});
