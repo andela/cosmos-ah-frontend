c20cb60f5ae951871c316b259c4b66831e5947fa
Tolulope Olaniyan <toluola7@gmail.com>
2019-05-09 12:54:16 +0100

    164796850-feature (loginReduxFunctionality): Setting up the Redux for login Functionality
     - A login redux action will be dispached
    
    [Delivers #164796850]

--- src/state/auth/actions.js
+++ src/state/auth/actions.js
@@ -1,9 +1,9 @@
-import { Signin } from '../../lib/signin';
 import { successDispatch, errorDispatch } from '../../lib/auth';
+import axios from '../../lib/axios';
 
 export const signin = async formData => {
   try {
-    const login = await Signin(formData);
+    const login = await axios.post('/login', formData);
     successDispatch(login.data.data.token);
   } catch (error) {
     errorDispatch(error.response);
